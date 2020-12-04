a40c116204a371cf73e7b28956cf6893141dafec
Abyan Burhanuddin Ahmad <abyanhmd2@gmail.com>
2020-12-04 18:08:15 +0700

    dialog box

--- lib/ui/pasien/tambahMakanan.dart
+++ lib/ui/pasien/tambahMakanan.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:prak_ppl/ui/dialog_box/dialogBox.dart';
 
 class TambahMakanan extends StatefulWidget {
   TambahMakanan({this.app});
@@ -13,6 +14,8 @@ class _TambahMakananState extends State<TambahMakanan> {
   TextEditingController nameController = TextEditingController();
   String namaMakanan = '';
   final referenceDatase = FirebaseDatabase.instance;
+  bool match = false ;
+  List<String> alergi = ['ayam', 'pecel', 'gulai'];
 
   @override
   void initState() {
@@ -49,6 +52,38 @@ class _TambahMakananState extends State<TambahMakanan> {
                     });
                   },
                 )),
+            RaisedButton(
+              child: Text('Cek Makanan'),
+              color: Colors.green,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16.0))),
+              onPressed: () {
+                for (String i in alergi) namaMakanan == i ? match = true : match = false;
+                if (match == true) {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return CustomDialogBox(
+                          title: "Cek Makanan",
+                          descriptions:
+                              "Makanan " + namaMakanan + ' mengandung bahan alergi',
+                          text: "OK",
+                        );
+                      });
+                } else if (match == false) {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return CustomDialogBox(
+                          title: "Cek Makanan",
+                          descriptions:
+                              "Makanan " + namaMakanan + ' aman, tidak mengandung alergi',
+                          text: "OK",
+                        );
+                      });
+                }
+              },
+            ),
             RaisedButton(
               child: Text('Tambah'),
               color: Colors.green,
